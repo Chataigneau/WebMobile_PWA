e5f54863454ac451937508a4c86a759bd2e9ec46
Nico <skywosh@gmail.com>
2020-12-17 15:14:33 +0100

    test sw

--- script.js
+++ script.js
@@ -24,11 +24,11 @@ var html = `
 <table>
   <tbody>
     <tr>
-      <td><img src='images/sonic.png' alt='sonic' style="max-width:100%;height:auto;"></td>
-      <td><img src='images/mario.png' alt='mario' style="max-width:100%;height:auto;"></td>
-      <td><img src='images/captain.png' alt='captain' style="max-width:100%;height:auto;"></td>
-      <td><img src='images/pika.png' alt='pikachu' style="max-width:100%;height:auto;"></td>
-      <td><img src='images/falco.png' alt='falco' style="max-width:100%;height:auto;"></td>
+      <td><img src='images/sonic.png' alt='sonic' class='photo'></td>
+      <td><img src='images/mario.png' alt='mario' class='photo'></td>
+      <td><img src='images/captain.png' alt='captain' class='photo'></td>
+      <td><img src='images/pika.png' alt='pikachu' class='photo'></td>
+      <td><img src='images/falco.png' alt='falco' class='photo'></td>
     </tr>
     <tr>
       <td><p class='characterName' style="font-size:2vw;"/>SONIC</p></td>
@@ -42,9 +42,9 @@ var html = `
 
 function viewImage(id) {
     var contenuImage=!afficher ? 
-    html : "<img src='images/ball.png' alt='ball'> ";
+    html : "<img src='images/ball.png' alt='ball' clas='photo'> ";
     afficher=!afficher;
-    var contenuBouton=afficher ? "<h2 style='font-size:2vw;'>Cacher les icones</h2>" : "<h2 style='font-size:2vw;''>Afficher les icones</h2>" ; 
+    var contenuBouton=afficher ? "<h2 style='font-size:2vw;'>Cacher les icones</h2>" : "<h2 style='font-size:2vw;'>Afficher les icones</h2>" ; 
      
     document.getElementById(id).innerHTML=contenuImage;
     document.getElementById("bouton").innerHTML=contenuBouton;
