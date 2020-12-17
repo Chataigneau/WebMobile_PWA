e735ce8e4bb5b7bb37f45f18997967e686b98c31
Nico <skywosh@gmail.com>
2020-12-17 14:45:01 +0100

    service worker

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+/*const sonicJson = {"name" : "Sonic" , "description" : "Icone de Sonic", "src" : "images/sonic.png"};
+var sonicObject = JSON.parse(sonicJson);
+document.getElementById("src").innerHTML = sonicObject.src;
+
+const MarioJson = {"name" : "Mario" , "description" : "Icone de Mario", "src" : "images/mario.png"};
+var marioObject = JSON.parse(JSON.stringify(marioJson));
+document.getElementById("src").innerHTML = marioObject.src;
+
+const CaptainJson = {"name" : "Captain Falcon" , "description" : "Icone de Captain Falcon", "src" : "images/captain.png"};
+var captainObject = JSON.parse(captainJson);
+document.getElementById("src").innerHTML = captainObject.src;
+
+const PikachuJson = {"name" : "Pikachu" , "description" : "Icone de Pikachu", "src" : "images/pika.png"};
+var pikachuObject = JSON.parse(pikachuJson);
+document.getElementById("src").innerHTML = pikachuObject.src;
+
+const FalcoJson = {"name" : "Falco" , "description" : "Icone de Falco", "src" : "images/falco.png"};
+var falcoObject = JSON.parse(falcoJson);
+document.getElementById("src").innerHTML = falcoObject.src;*/
+
 var afficher=false; 
 var html = `
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -24,7 +44,7 @@ function viewImage(id) {
     var contenuImage=!afficher ? 
     html : "<img src='images/ball.png' alt='ball'> ";
     afficher=!afficher;
-    var contenuBouton=afficher ? "<h2>Cacher les icones</h2>" : "<h2>Afficher les icones</h2>" ; 
+    var contenuBouton=afficher ? "<h2 style='font-size:2vw;'>Cacher les icones</h2>" : "<h2 style='font-size:2vw;''>Afficher les icones</h2>" ; 
      
     document.getElementById(id).innerHTML=contenuImage;
     document.getElementById("bouton").innerHTML=contenuBouton;
