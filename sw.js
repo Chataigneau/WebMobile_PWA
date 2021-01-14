b59d54fc87f469aa684174e3a576b82708c86d6d
Nico <skywosh@gmail.com>
2021-01-14 10:50:16 +0100

    Add cloudflare

--- sw.js
+++ sw.js
@@ -7,9 +7,10 @@ const cacheName = "galerie-v1";
 const files = [
   "/",
   "/script.js",
-  "https://nicolaswebmobilepwa.netlify.app/images/sonic.png",
+  /*"https://nicolaswebmobilepwa.netlify.app/images/sonic.png",
   "https://nicolaswebmobilepwa.netlify.app/images/mario.png",
   "https://nicolaswebmobilepwa.netlify.app/images/captain.png",
   "https://nicolaswebmobilepwa.netlify.app/images/pika.png",
-  "https://nicolaswebmobilepwa.netlify.app/images/falco.png"
+  "https://nicolaswebmobilepwa.netlify.app/images/falco.png",*/
+  "https://cdnjs.cloudflare.com/ajax/libs/localforage/1.7.3/localforage.min.js"
 ];
